61b4bdd218ca0f9cd1e5dd1880e9824215344a6c
Max <mrliucan@foxmail.com>
2019-02-07 22:07:52 +1100

    refactor: resolve some issues brought up by tslint

--- src/OverrideMaterialUICss.tsx
+++ src/OverrideMaterialUICss.tsx
@@ -1,5 +1,5 @@
-import { createGenerateClassName, jssPreset } from "@material-ui/core/styles"
 import CssBaseline from "@material-ui/core/CssBaseline"
+import { createGenerateClassName, jssPreset } from "@material-ui/core/styles"
 import { create } from "jss"
 import * as React from "react"
 import JssProvider from "react-jss/lib/JssProvider"
@@ -7,9 +7,8 @@ import JssProvider from "react-jss/lib/JssProvider"
 const jssInsertionPoint = "jss-insertion-point"
 
 const isInsertionPointExisting = (): boolean => {
-  const documentHead = document.head
-  for (var i = 0; i < documentHead.childNodes.length; i++) {
-    var node = documentHead.childNodes[i]
+  const childNodesArray = Array.from(document.head.childNodes)
+  for (const node of childNodesArray) {
     if (node.nodeType === 8 && node.nodeValue === jssInsertionPoint) {
       return true
     }
@@ -19,14 +18,13 @@ const isInsertionPointExisting = (): boolean => {
 
 const insertInsertionPoint = () => {
   const styleNode = document.createComment(jssInsertionPoint)
-  const documentHead = document.head as HTMLHeadElement
+  const documentHead = document.head
   documentHead.insertBefore(styleNode, documentHead.firstChild)
 }
 
 const removeInsertionPoint = () => {
-  const documentHead = document.head
-  for (var i = 0; i < documentHead.childNodes.length; i++) {
-    var node = documentHead.childNodes[i]
+  const childNodesArray = Array.from(document.head.childNodes)
+  for (const node of childNodesArray) {
     if (node.nodeType === 8 && node.nodeValue === jssInsertionPoint) {
       node.remove()
     }
@@ -40,14 +38,15 @@ const setupInsertionPoint = () => {
 
   const generateClassName = createGenerateClassName()
   const jss = create(jssPreset())
-  // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+  // We define a custom insertion point that JSS will look for injecting the
+  // styles in the DOM.
   jss.setup({ insertionPoint: "jss-insertion-point" })
 
   return { generateClassName, jss }
 }
 
 type Props = {
-  children?: (JSX.Element | null)[] | JSX.Element
+  children?: Array<JSX.Element | null> | JSX.Element
   useCssBaseline?: boolean
 }
 type State = { generateClassName; jss }
@@ -58,11 +57,11 @@ class OverrideMaterialUICss extends React.Component<Props, State> {
     this.state = setupInsertionPoint()
   }
 
-  componentWillUnmount() {
+  componentWillUnmount(): void {
     removeInsertionPoint()
   }
 
-  render() {
+  render(): JSX.Element {
     const { generateClassName, jss } = this.state
     const { children, useCssBaseline = true } = this.props
 
